93b046f029102c26be081513029732cd1be1f71b
nakajijapan <nakajima@paperboy.co.jp>
2013-02-12 04:04:19 +0900

    move function to Base Class

--- lib/api_bucket/base/client/http.rb
+++ lib/api_bucket/base/client/http.rb
@@ -2,10 +2,19 @@ module ApiBucket::Base
   module Client
     module Http
       def send_request(options, url)
-        request_url = "#{url}?#{ApiBucket::Base::Client::Http::prepare_query(options)}"
-        uri = URI::parse(request_url)
-        res = Net::HTTP.get_response(uri)
-        res.body
+        request_url = "#{url}?#{prepare_query(options)}"
+
+        res = nil
+        begin
+          res = open(request_url, {}) do |f|
+            @raw_response = f.read
+            JSON.parse(@raw_response)
+          end
+        rescue => e
+          #p "#{e.message} : #{request_url}"
+        end
+
+        res
       end
 
       def prepare_query(options)
--- lib/api_bucket/itunes/client/http.rb
+++ lib/api_bucket/itunes/client/http.rb
@@ -4,22 +4,6 @@ module ApiBucket::Itunes
   class Client
     module Http
       include ApiBucket::Base::Client::Http
-
-      def send_request(options, url)
-        request_url = "#{url}?#{prepare_query(options)}"
-
-        res = nil
-        begin
-          res = open(request_url, {}) do |f|
-            @raw_response = f.read
-            JSON.parse(@raw_response)
-          end
-        rescue => e
-          #p "#{e.message} : #{request_url}"
-        end
-
-        res
-      end
     end
   end
 end
